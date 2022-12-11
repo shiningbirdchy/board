553c0f72da98bcff33b2e1c919a8f41bdf8b7aa3
shiningbirdchy <117141183+shiningbirdchy@users.noreply.github.com>
2022-12-11 21:47:13 +0900

    Delete azure-cosmos-java-sample2 directory

--- azure-cosmos-java-sample2/src/com/microsoft/azure/cosmos/sample/dao/TodoDaoFactory.java
+++
@@ -1,9 +0,0 @@
-package com.microsoft.azure.cosmos.sample.dao;
-
-public class TodoDaoFactory {
-	private static TodoDao myTodoDao = new DocDbDao();
-
-	public static TodoDao getDao() {
-		return myTodoDao;
-	}
-}
